5f3d4bc00ea3b6871cecaba371f056655017b806
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2023-05-21 09:33:11 +0300

    refactor: update app dirs and configs

---
+++ api/jest.config.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '.env.test' })
+
+import type { Config } from 'jest';
+
+export default async (): Promise<Config> => {
+  return {
+    verbose: true,
+    silent: true,
+    testTimeout: 5000,
+    detectOpenHandles: true,
+    openHandlesTimeout: 3000,
+    testEnvironment: 'node',
+    preset: 'ts-jest',
+  };
+};
